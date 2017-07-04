bc127e8cf82bf69fea34eb9fd570c1009225a3a9
Exo <k.m.epvp@gmail.com>
2017-07-04 20:54:04 +0200

    remove shit

--- Abdelrahman Hagrass_files/9YFgztm6Ef6.js
+++
@@ -1,5 +0,0 @@
-/*!CK:376812456!*//*1443541554,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["CssN+"]); }
-
-__d('MHeaderSelect',['Stratcom','MViewport'],function a(b,c,d,e,f,g,h,i){if(c.__markCompiled)c.__markCompiled();h.listen('focus','header-select',i.scrollToHeader);},null);
